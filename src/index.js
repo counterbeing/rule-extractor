80b0eef737617e2aa1f251dcfe093658eba10904
Cory Logan <he@corylogan.com>
2017-11-09 19:46:44 -0800

    optional escaping

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import fs from 'fs'
 
 const selectorReg = /^##/
 
-export default (rulesFile) => {
+export default (rulesFile, escape=true) => {
   let lines = fs.readFileSync(rulesFile).toString().split('\n')
   return lines.map(function(el) {
     if(el.match(selectorReg)) {
-      return el.replace(selectorReg, '').replace(/"/g, '\\"') }
+      el = el.replace(selectorReg, '')
+      if(!escape) return  el
+      return el.replace(/"/g, '\\"') }
   }).filter((el) => {
     return typeof(el) == 'string' &&
       el.length > 1 &&
